ee0a7dd42810825f07a830c8fb233880fd7d65e0
IMLHF <red_wind@foxmail.com>
2019-03-06 22:14:44 +0800

    # updata test

--- 2_test_CC_and_OC.py
+++ 2_test_CC_and_OC.py
@@ -8,6 +8,7 @@ from utils import audio_tool
 from pypesq import pesq
 from models.lstm_SE import SE_MODEL
 from FLAGS import PARAM
+import math
 from dataManager.mixed_aishell_8k_tfrecord_io import generate_tfrecord, get_batch_use_tfdata
 
 def _build_model_use_tfdata(test_set_tfrecords_dir, ckpt_dir):
@@ -19,7 +20,7 @@ def _build_model_use_tfdata(test_set_tfrecords_dir, ckpt_dir):
     # region TFRecord+DataSet
     with tf.device('/cpu:0'):
       with tf.name_scope('input'):
-        x_batch, y_batch, Xtheta_batch, Ytheta_batch, lengths_batch, iter_train = get_batch_use_tfdata(
+        x_batch, y_batch, Xtheta_batch, Ytheta_batch, lengths_batch, iter_test = get_batch_use_tfdata(
           test_set_tfrecords_dir,
           get_theta=True)
 
@@ -49,7 +50,7 @@ def _build_model_use_tfdata(test_set_tfrecords_dir, ckpt_dir):
       tf.logging.fatal("checkpoint not found.")
       sys.exit(-1)
   g.finalize()
-  return sess, test_model
+  return sess, test_model, iter_test
 
 
 def get_PESQ_STOI_SDR(test_set_tfrecords_dir, ckpt_dir):
@@ -59,31 +60,67 @@ def get_PESQ_STOI_SDR(test_set_tfrecords_dir, ckpt_dir):
   y_ : clean(label) audio's ...
   y_xx_est " estimate audio's ...
   '''
-  sess, model = _build_model_use_tfdata(test_set_tfrecords_dir, ckpt_dir)
-  mask, x_mag, x_theta, y_mag, y_theta, y_mag_est = sess.run([model.mask,
-                                                              model.inputs,
-                                                              model.x_theta,
-                                                              model.labels,
-                                                              model.y_theta,
-                                                              model.cleaned])
-  x_wav = spectrum_tool.librosa_istft(x_mag*np.exp(1j*x_theta), PARAM.NFFT, PARAM.OVERLAP)
-  y_wav = spectrum_tool.librosa_istft(y_mag*np.exp(1j*y_theta), PARAM.NFFT, PARAM.OVERLAP)
-  if PARAM.RESTORE_PHASE == 'MIXED':
-    y_spec_est = y_mag_est*np.exp(1j*x_theta)
-    y_wav_est = spectrum_tool.librosa_istft(y_spec_est, PARAM.NFFT, PARAM.OVERLAP)
-  elif PARAM.RESTORE_PHASE =='GRIFFIN_LIM':
-    y_wav_est = spectrum_tool.griffin_lim(y_mag_est,
-                                          PARAM.NFFT,
-                                          PARAM.OVERLAP,
-                                          PARAM.GRIFFIN_ITERNUM,
-                                          x_wav)
-  pesq_ans = audio_tool.get_batch_pesq_improvement(x_wav, y_wav, y_wav_est)
-  stoi_ans = audio_tool.get_batch_stoi_improvement(x_wav, y_wav, y_wav_est)
-  sdr_ans = audio_tool.get_batch_sdr_improvement(x_wav, y_wav, y_wav_est)
-  return pesq_ans, stoi_ans, sdr_ans
+  sess, model, iter_test = _build_model_use_tfdata(test_set_tfrecords_dir, ckpt_dir)
+  sess.run(iter_test.initializer)
+  i = 0
+  all_batch = math.ceil(PARAM.DATASET_SIZES[-1] / PARAM.batch_size)
+  pesq_mat = None
+  stoi_mat = None
+  sdr_mat = None
+  while True:
+    try:
+      i += 1
+      print("Testing batch %03d/%03d: " % (i,all_batch))
+      mask, x_mag, x_theta, y_mag, y_theta, y_mag_est = sess.run([model.mask,
+                                                                  model.inputs,
+                                                                  model.x_theta,
+                                                                  model.labels,
+                                                                  model.y_theta,
+                                                                  model.cleaned])
+      x_wav = spectrum_tool.librosa_istft(x_mag*np.exp(1j*x_theta), PARAM.NFFT, PARAM.OVERLAP)
+      y_wav = spectrum_tool.librosa_istft(y_mag*np.exp(1j*y_theta), PARAM.NFFT, PARAM.OVERLAP)
+      if PARAM.RESTORE_PHASE == 'MIXED':
+        y_spec_est = y_mag_est*np.exp(1j*x_theta)
+        y_wav_est = spectrum_tool.librosa_istft(y_spec_est, PARAM.NFFT, PARAM.OVERLAP)
+      elif PARAM.RESTORE_PHASE =='GRIFFIN_LIM':
+        y_wav_est = [spectrum_tool.griffin_lim(y_mag_est_t,
+                                               PARAM.NFFT,
+                                               PARAM.OVERLAP,
+                                               PARAM.GRIFFIN_ITERNUM,
+                                               x_wav_t) for y_mag_est_t, x_wav_t in zip(y_mag_est, x_wav)]
+      print('  |-Calculating PESQ...')
+      pesq_mat_t = audio_tool.get_batch_pesq_improvement(x_wav, y_wav, y_wav_est)
+      print('  |-Calculating STOI...')
+      stoi_mat_t = audio_tool.get_batch_stoi_improvement(x_wav, y_wav, y_wav_est)
+      print('  |-Calculating SDR...')
+      sdr_mat_t = audio_tool.get_batch_sdr_improvement(x_wav, y_wav, y_wav_est)
+      if pesq_mat is None:
+        pesq_mat = pesq_mat_t
+        stoi_mat = stoi_mat_t
+        sdr_mat = sdr_mat_t
+      else:
+        pesq_mat = np.concatenate((pesq_mat,pesq_mat_t),axis=-1)
+        stoi_mat = np.concatenate((stoi_mat,stoi_mat_t),axis=-1)
+        sdr_mat = np.concatenate((sdr_mat,sdr_mat_t),axis=-1)
+    except tf.errors.OutOfRangeError:
+      break
+  pesq_ans = np.mean(pesq_mat,axis=-1)
+  stoi_ans = np.mean(stoi_mat,axis=-1)
+  sdr_ans = np.mean(sdr_mat,axis=-1)
+  return {'pesq':pesq_ans, 'stoi':stoi_ans, 'sdr':sdr_ans}
 
 def test_CC_and_OC():
   ckpt_dir = PARAM.CHECK_POINT
   _, _, testcc_tfrecords_dir, testoc_tfrecords_dir = generate_tfrecord(
       gen=PARAM.GENERATE_TFRECORD)
   pesq_ans, stoi_ans, sdr_ans = get_PESQ_STOI_SDR(testcc_tfrecords_dir, ckpt_dir)
+  print(pesq_ans)
+  print(stoi_ans)
+  print(sdr_ans)
+  with open('test_ans.log','a+') as f:
+    f.write(pesq_ans)
+    f.write('\n')
+    f.write(stoi_ans)
+    f.write('\n')
+    f.write(sdr_ans)
+    f.write('\n')
