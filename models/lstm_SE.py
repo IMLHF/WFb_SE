5d3bfbc85c1f6221c844c238edad6af4e931aa7b
IMLHF <red_wind@foxmail.com>
2019-03-08 19:14:55 +0800

    #

--- models/lstm_SE.py
+++ models/lstm_SE.py
@@ -71,10 +71,9 @@ class SE_MODEL(object):
     self._norm_x_mag_spec = norm_mag_spec(self._x_mag_spec)
     self._norm_x_logmag_spec = norm_logmag_spec(self._x_mag_spec, self._log_bias)
 
-    if y_mag_spec_batch is not None:
-      self._y_mag_spec = y_mag_spec_batch
-      self._norm_y_mag_spec = norm_mag_spec(self._y_mag_spec)
-      self._norm_y_logmag_spec = norm_logmag_spec(self._y_mag_spec, self._log_bias)
+    self._y_mag_spec = y_mag_spec_batch
+    self._norm_y_mag_spec = norm_mag_spec(self._y_mag_spec)
+    self._norm_y_logmag_spec = norm_logmag_spec(self._y_mag_spec, self._log_bias)
 
     self._lengths = lengths_batch
     self._batch_size = tf.shape(self._lengths)[0]
@@ -87,11 +86,10 @@ class SE_MODEL(object):
       self.net_input = self._norm_x_mag_spec
     elif PARAM.INPUT_TYPE == 'logmag':
       self.net_input = self._norm_x_logmag_spec
-    if y_mag_spec_batch is not None:
-      if PARAM.LABEL_TYPE == 'mag':
-        self._labels = self._norm_y_mag_spec
-      elif PARAM.LABEL_TYPE == 'logmag':
-        self._labels = self._norm_y_logmag_spec
+    if PARAM.LABEL_TYPE == 'mag':
+      self._labels = self._norm_y_mag_spec
+    elif PARAM.LABEL_TYPE == 'logmag':
+      self._labels = self._norm_y_logmag_spec
 
     outputs = self.net_input
 
