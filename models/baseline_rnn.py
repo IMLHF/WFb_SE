92d066beded64e2350d6ad42162345d8554c0962
IMLHF <red_wind@foxmail.com>
2019-03-10 22:43:29 +0800

    # fix y_mag_label

--- models/baseline_rnn.py
+++ models/baseline_rnn.py
@@ -176,7 +176,7 @@ class Model_Baseline(object):
       self._loss = loss.reduce_sum_frame_batchsize_MSE(self._y_estimation,self._y_labels)
     elif FLAGS.PARAM.LOSS_FUNC == 'MFCC_SPEC_MSE':
       self._loss1, self._loss2 = loss.reduce_sum_frame_batchsize_MFCC_AND_SPEC_MSE(self._y_estimation, self._y_labels,
-                                                                                   self._y_mag_estimation, self._x_mag_spec)
+                                                                                   self._y_mag_estimation, self._y_mag_spec)
       self._loss = FLAGS.PARAM.SPEC_LOSS_COEF*self._loss1 + FLAGS.PARAM.MFCC_LOSS_COEF*self._loss2
     # endregion
 
