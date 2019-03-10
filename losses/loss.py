193d160e77b4a966ab467f8b4bee575fd5c8217b
IMLHF <red_wind@foxmail.com>
2019-03-10 19:21:17 +0800

    # add mfcc_spec loss

--- losses/loss.py
+++ losses/loss.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import FLAGS
+from utils import tf_tool
 
 def reduce_sum_frame_batchsize_MSE(y1, y2):
   cost = tf.reduce_mean(tf.reduce_sum(tf.pow(y1-y2, 2), 1), 1)
@@ -14,3 +15,16 @@ def reduce_sum_frame_batchsize_MSE_LOW_FS_IMPROVE(y1, y2):
   loss2 = reduce_sum_frame_batchsize_MSE(tf.slice(y1, [0, 0, 0], [-1, -1, low_frame_point]),
                                          tf.slice(y2, [0, 0, 0], [-1, -1, low_frame_point]))
   return loss1+loss2
+
+def reduce_sum_frame_batchsize_MFCC_ANS_SPEC_MSE(spec_est,spec_label):
+  '''
+  spec_est:
+    dim: [batch,time,frequence]
+  spec_label:
+    dim: [batch,time,frequence]
+  '''
+  mfccs_est = tf_tool.mfccs_form_realStft(spec_est, FLAGS.PARAM.FS, 20, 13)
+  mfccs_label = tf_tool.mfccs_form_realStft(spec_label, FLAGS.PARAM.FS, 20, 13)
+  loss1 = reduce_sum_frame_batchsize_MSE(mfccs_est, mfccs_label)
+  loss2 = reduce_sum_frame_batchsize_MSE(spec_est, spec_label)
+  return 0.5*loss1+0.5*loss2
