2bb6c926cfa0c5dc46da79c01260712280145776
IMLHF <red_wind@foxmail.com>
2019-03-13 17:16:37 +0800

    # fix model

--- models/baseline_rnn.py
+++ models/baseline_rnn.py
@@ -10,47 +10,7 @@ from utils.tf_tool import norm_mag_spec, norm_logmag_spec, rm_norm_mag_spec, rm_
 from utils.tf_tool import normedLogmag2normedMag, normedMag2normedLogmag
 from utils.tf_tool import lstm_cell, GRU_cell
 import FLAGS
-
-
-def threshold_feature(features, inputs, input_finnal_dim):
-  '''
-  inputs: func inputs of calculate noise_threshold
-  '''
-  in_size = input_finnal_dim
-  with tf.variable_scope('fullconnectGetThreshold'):
-    # attention layer
-    with tf.variable_scope('attention_scorer'):
-      weights_scorer = tf.get_variable('weights_scorer', [in_size, 1],
-                                       initializer=tf.random_normal_initializer(stddev=0.01))
-      biases_scorer = tf.get_variable('biases_scorer', [1],
-                                      initializer=tf.constant_initializer(0.0))
-      attention_alpha_vec = tf.matmul(inputs, weights_scorer) + biases_scorer # [batch,time,1]
-      attention_alpha_vec = tf.expand_dims(tf.squeeze(attention_alpha_vec, axis=[-1]),-2) # [batch,1,time]
-      attention_alpha_vec = tf.nn.softmax(attention_alpha_vec, axis=-1)
-      attened_vec = tf.squeeze(tf.matmul(attention_alpha_vec, inputs), axis=[-2]) # [batch, in_size]
-
-    weights_threshold = tf.get_variable('weights_threshold', [in_size, 1],
-                                        initializer=tf.random_normal_initializer(stddev=0.01))
-    biases_threshold = tf.get_variable('biases_threshold', [1],
-                                       initializer=tf.constant_initializer(0.0))
-    noise_mag_threshold = tf.relu(tf.matmul(attened_vec, weights_threshold) + biases_threshold)
-
-
-    if FLAGS.PARAM.THRESHOLD_FUNC == FLAGS.PARAM.SQUARE_FADE:
-      tf.logging.info('Use Square Noise Threshold.')
-      features = tf.multiply(features, tf.nn.relu6(features*6/noise_mag_threshold) / 6)
-      return features
-    elif FLAGS.PARAM.THRESHOLD_FUNC == FLAGS.PARAM.EXPONENTIAL_FADE:
-      tf.logging.info('Use Exponential Noise Threshold.')
-      features = tf.pow(features/noise_mag_threshold, 2 - tf.nn.relu6(features*6/noise_mag_threshold)/6)
-      return features
-    elif FLAGS.PARAM.THRESHOLD_FUNC == FLAGS.PARAM.EN_EXPONENTIAL_FADE:
-      tf.logging.info('Use Enhanced Exponential Noise Threshold.')
-      features = tf.pow(features/noise_mag_threshold, (2 - tf.nn.relu6(features*6/noise_mag_threshold)/6)**2)
-      return features
-    else:
-      print('Threshold error.')
-      exit(-1)
+import numpy as np
 
 
 class Model_Baseline(object):
@@ -163,8 +123,8 @@ class Model_Baseline(object):
       rnn_output_num = FLAGS.PARAM.RNN_SIZE*2
       if FLAGS.PARAM.MODEL_TYPE == 'BLSTM' and (not (FLAGS.PARAM.LSTM_num_proj is None)):
         rnn_output_num = 2*FLAGS.PARAM.LSTM_num_proj
-      outputs = tf.reshape(outputs, [-1, rnn_output_num])
       in_size = rnn_output_num
+    outputs = tf.reshape(outputs, [-1, in_size])
     out_size = FLAGS.PARAM.OUTPUT_SIZE
     with tf.variable_scope('fullconnectOut'):
       weights = tf.get_variable('weights1', [in_size, out_size],
@@ -177,28 +137,19 @@ class Model_Baseline(object):
                                        initializer=tf.random_normal_initializer(mean=1.0, stddev=0.01))
         biases_coef = tf.get_variable('biases_coef', [1],
                                       initializer=tf.constant_initializer(0.0))
-      mask = tf.multiply(tf.nn.softmax(tf.reshape(tf.matmul(outputs, weights) + biases, [self._batch_size,-1])),
-                         tf.nn.relu(tf.reduce_sum(tf.matmul(outputs, weights_coef) + biases_coef, axis=[-1,-2])))
+      raw_mask = tf.reshape(tf.matmul(outputs, weights) + biases, [self._batch_size,-1]) # [batch, time*fre]
+      batch_coef_vec = tf.reshape(tf.matmul(outputs, weights_coef) + biases_coef, [self._batch_size,-1]) # [batch, time]
+      batch_coef = tf.reduce_sum(batch_coef_vec, axis=[-1])
+      print(np.shape(batch_coef_vec),np.shape(batch_coef))
+      mask = tf.multiply(tf.nn.softmax(raw_mask,axis=-1),
+                         tf.nn.relu(batch_coef))
     else:
       mask = tf.nn.relu(tf.matmul(outputs, weights) + biases)
-    # endregion
-
-    # region Apply Noise Threshold Function on Mask
-    if FLAGS.PARAM.THRESHOLD_FUNC is not None:
-      # use noise threshold
-      if FLAGS.PARAM.THRESHOLD_POS == FLAGS.PARAM.THRESHOLD_ON_MASK:
-        mask = threshold_feature(mask, outputs, in_size)
-      elif FLAGS.PARAM.THRESHOLD_POS == FLAGS.PARAM.THRESHOLD_ON_SPEC:
-        pass
-      else:
-        print('Threshold position error!')
-        exit(-1)
     # endregion
 
     self._mask = tf.reshape(
         mask, [self._batch_size, -1, FLAGS.PARAM.OUTPUT_SIZE])
 
-    self.saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=30)
 
     # region get infer spec
     if FLAGS.PARAM.DECODING_MASK_POSITION == 'mag':
@@ -226,18 +177,6 @@ class Model_Baseline(object):
       tf.logging.error('Mask type error.')
       exit(-1)
 
-    # region Apply Noise Threshold Function on Spec(log or mag)
-    if FLAGS.PARAM.THRESHOLD_FUNC is not None:
-      # use noise threshold
-      if FLAGS.PARAM.THRESHOLD_POS == FLAGS.PARAM.THRESHOLD_ON_MASK:
-        pass
-      elif FLAGS.PARAM.THRESHOLD_POS == FLAGS.PARAM.THRESHOLD_ON_SPEC:
-        self._y_estimation = threshold_feature(self._y_estimation, outputs, in_size)
-      else:
-        print('Threshold position error!')
-        exit(-1)
-    # endregion
-
     if FLAGS.PARAM.TRAINING_MASK_POSITION != FLAGS.PARAM.LABEL_TYPE:
       if FLAGS.PARAM.LABEL_TYPE == 'mag':
         self._y_estimation = normedLogmag2normedMag(self._y_estimation, FLAGS.PARAM.MAG_NORM_MAX,
@@ -250,6 +189,7 @@ class Model_Baseline(object):
     if behavior == Model_Baseline.infer:
       return
 
+    self.saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=30)
     # region get LOSS
     if FLAGS.PARAM.LOSS_FUNC == 'SPEC_MSE': # log_mag and mag MSE
       self._loss = loss.reduce_sum_frame_batchsize_MSE(self._y_estimation,self._y_labels)
