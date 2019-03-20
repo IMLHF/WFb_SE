dc1e8087088b6901c826b99b78c541c1c4ad2492
IMLHF <red_wind@foxmail.com>
2019-03-20 18:42:29 +0800

    # add training in ture

---
+++ 1_alter_train.py
@@ -0,0 +1,255 @@
+import time
+import tensorflow as tf
+import numpy as np
+import sys
+import utils
+import utils.audio_tool
+import os
+import shutil
+import wave
+import gc
+from FLAGS import PARAM
+from tensorflow.python import debug as tf_debug
+from dataManager.mixed_aishell_8k_tfrecord_io import generate_tfrecord, get_batch_use_tfdata
+
+
+
+def train_one_epoch(sess, tr_model):
+  """Runs the model one epoch on given data."""
+  tr_mag_loss, tr_logmag_loss, i = 0, 0, 0
+  stime = time.time()
+  while True:
+    try:
+      # sess = tf_debug.LocalCLIDebugWrapperSession(sess, ui_type='curses')
+      _, mag_loss, logmag_loss, current_batchsize, log_bias, logbiasnet_lr, masknet_lr = sess.run(
+          # [tr_model.train_op, tr_model.loss, tf.shape(tr_model.lengths)[0]])
+          [tr_model.train_op,
+           tr_model.logbiasnet_loss,
+           tr_model.masknet_loss,
+           tr_model.batch_size,
+           tr_model.log_bias,
+           tr_model.lr_logbiasnet,
+           tr_model.lr_masknet
+           #tr_model.threshold
+           ])
+      tr_mag_loss += mag_loss
+      tr_logmag_loss += logmag_loss
+      print(mag_loss, logmag_loss,flush=True)
+      print(np.min(log_bias),np.max(log_bias),np.mean(log_bias),np.sqrt(np.var(log_bias)),flush=True)
+      # print(np.min(threshold),np.max(threshold),np.mean(threshold),np.sqrt(np.var(threshold)),flush=True)
+      # print("\r    Batch loss: %f" % loss, end="")
+      if (i+1) % PARAM.minibatch_size == 0:
+        costtime = time.time()-stime
+        stime = time.time()
+        avg_mag_loss = tr_mag_loss / (i+1)
+        avg_logmag_loss = tr_logmag_loss / (i+1)
+        train_epoch_msg = ("MINIBATCH %05d: magLOSS %04.6f, logmagLOSS %04.6f, "
+                           "LR (%02.6f,%02.6f), AVG.log_bias %05.2f, "
+                           "DURATION %06dS") % (
+                               i + 1, avg_mag_loss, avg_logmag_loss, logbiasnet_lr, masknet_lr, np.mean(log_bias), costtime)
+        tf.logging.info(train_epoch_msg)
+        with open(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT+'_train.log'), 'a+') as f:
+          f.writelines(train_epoch_msg+'\n')
+        sys.stdout.flush()
+      i += 1
+    except tf.errors.OutOfRangeError:
+      break
+  tr_mag_loss /= i
+  tr_logmag_loss /= i
+  return tr_mag_loss, tr_logmag_loss, logbiasnet_lr, masknet_lr, log_bias
+
+
+def eval_one_epoch(sess, val_model):
+  """Cross validate the model on given data."""
+  val_mag_loss, val_logmag_loss = 0, 0
+  data_len = 0
+  while True:
+    try:
+      mag_loss, logmag_loss, current_batchsize = sess.run([
+          val_model.logbiasnet_loss, val_model.masknet_loss, val_model.batch_size
+          # val_model.threshold,
+          # val_model._loss1, val_model._loss2,
+      ])
+      # print(inputs)
+      # exit(0)
+      # print(loss2,'/',loss1)
+      sys.stdout.flush()
+      val_mag_loss += mag_loss
+      val_logmag_loss += logmag_loss
+      # print(np.min(log_bias),np.max(log_bias),np.mean(log_bias),np.sqrt(np.var(log_bias)),flush=True)
+      # print(np.min(threshold),np.max(threshold),np.mean(threshold),np.sqrt(np.var(threshold)),flush=True)
+      # print("\r    Batch loss: %f" % loss, end="")
+      data_len += 1
+    except tf.errors.OutOfRangeError:
+      break
+  val_mag_loss /= data_len
+  val_logmag_loss /= data_len
+  return val_mag_loss, val_logmag_loss
+
+
+def train():
+
+  g = tf.Graph()
+  with g.as_default():
+    # region TFRecord+DataSet
+    with tf.device('/cpu:0'):
+      with tf.name_scope('input'):
+        train_tfrecords, val_tfrecords, _, _ = generate_tfrecord(
+            gen=PARAM.GENERATE_TFRECORD)
+        if PARAM.GENERATE_TFRECORD:
+          print("TFRecords preparation over.")
+          # exit(0)  # set gen=True and exit to generate tfrecords
+
+        PSIRM = True if PARAM.MASK_TYPE == 'PSM' else False
+        x_batch_tr, y_batch_tr, Xtheta_batch_tr, Ytheta_batch_tr, lengths_batch_tr, iter_train = get_batch_use_tfdata(
+            train_tfrecords,
+            get_theta=PSIRM)
+        x_batch_val, y_batch_val,  Xtheta_batch_val, Ytheta_batch_val, lengths_batch_val, iter_val = get_batch_use_tfdata(
+            val_tfrecords,
+            get_theta=PSIRM)
+    # endregion
+
+    # build model
+    with tf.name_scope('model'):
+      tr_model = PARAM.SE_MODEL(x_batch_tr,
+                                lengths_batch_tr,
+                                y_batch_tr,
+                                Xtheta_batch_tr,
+                                Ytheta_batch_tr,
+                                PARAM.SE_MODEL.train)
+      tf.get_variable_scope().reuse_variables()
+      val_model = PARAM.SE_MODEL(x_batch_val,
+                                 lengths_batch_val,
+                                 y_batch_val,
+                                 Xtheta_batch_val,
+                                 Ytheta_batch_val,
+                                 PARAM.SE_MODEL.validation)
+
+    utils.tf_tool.show_all_variables()
+    init = tf.group(tf.global_variables_initializer(),
+                    tf.local_variables_initializer())
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = PARAM.GPU_RAM_ALLOW_GROWTH
+    config.allow_soft_placement = False
+    sess = tf.Session(config=config)
+    sess.run(init)
+
+    # region resume training
+    if PARAM.resume_training.lower() == 'true':
+      ckpt = tf.train.get_checkpoint_state(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT))
+      if ckpt and ckpt.model_checkpoint_path:
+        # tf.logging.info("restore from" + ckpt.model_checkpoint_path)
+        tr_model.saver.restore(sess, ckpt.model_checkpoint_path)
+        best_path = ckpt.model_checkpoint_path
+      else:
+        tf.logging.fatal("checkpoint not found")
+      with open(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT+'_train.log'), 'a+') as f:
+        f.writelines('Training resumed.\n')
+    else:
+      if os.path.exists(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT+'_train.log')):
+        os.remove(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT+'_train.log'))
+    # endregion
+
+    # region validation before training.
+    valstart_time = time.time()
+    sess.run(iter_val.initializer)
+    mag_loss_prev, logmag_loss_prev = eval_one_epoch(sess,
+                                                     val_model)
+    cross_val_msg = "CROSSVAL PRERUN LOGBIASNET_LOSS_MASKNET_LOSS (%.4F,%.4F),  Costime %dS" % (
+        mag_loss_prev, logmag_loss_prev, time.time()-valstart_time)
+    tf.logging.info(cross_val_msg)
+    with open(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT+'_train.log'), 'a+') as f:
+      f.writelines(cross_val_msg+'\n')
+
+    tr_model.assign_lr(sess, PARAM.learning_rate_logbiasnet, PARAM.learning_rate_masknet)
+    g.finalize()
+    # endregion
+
+    # epochs training
+    reject_num = 0
+    for epoch in range(PARAM.start_epoch, PARAM.max_epochs):
+      sess.run([iter_train.initializer, iter_val.initializer])
+      start_time = time.time()
+
+      # train one epoch
+      tr_mag_loss, tr_logmag_loss, logbiasnet_lr, masknet_lr, log_bias = train_one_epoch(sess,
+                                                                                         tr_model)
+      # Validation
+      val_mag_loss, val_logmag_loss = eval_one_epoch(sess,
+                                                     val_model)
+      end_time = time.time()
+
+      # Determine checkpoint path
+      ckpt_name = "nnet_iter%d_lrate(%e,%e)_trloss(%.4f,%.4f)_cvloss(%.4f,%.4f)_avglogbias%f_duration%ds" % (
+          epoch + 1, logbiasnet_lr, masknet_lr, tr_mag_loss, tr_logmag_loss, val_mag_loss, val_logmag_loss, np.mean(log_bias), end_time - start_time)
+      ckpt_dir = os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT)
+      if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+      ckpt_path = os.path.join(ckpt_dir, ckpt_name)
+
+      # Relative loss between previous and current val_loss
+      rel_impr = np.abs(mag_loss_prev - val_mag_loss) / mag_loss_prev
+      # Accept or reject new parameters
+      msg = ""
+      if val_mag_loss < mag_loss_prev:
+        reject_num = 0
+        tr_model.saver.save(sess, ckpt_path)
+        # Logging train loss along with validation loss
+        mag_loss_prev = val_mag_loss
+        best_path = ckpt_path
+        msg = ("Train Iteration %03d: \n"
+               "    Train.LOSS (%.4f,%.4f), lrate(%e,%e), Val.LOSS (%.4f,%.4f), avglog_bias %f,\n"
+               "    %s, ckpt(%s) saved,\n"
+               "    EPOCH DURATION: %.2fs\n") % (
+            epoch + 1,
+            tr_mag_loss, tr_logmag_loss, logbiasnet_lr, masknet_lr, val_mag_loss, val_logmag_loss, np.mean(log_bias),
+            "NNET Accepted", ckpt_name, end_time - start_time)
+        tf.logging.info(msg)
+      else:
+        reject_num += 1
+        tr_model.saver.restore(sess, best_path)
+        msg = ("Train Iteration %03d: \n"
+               "    Train.LOSS (%.4f,%.4f), lrate(%e,%e), Val.LOSS (%.4f,%.4f), avglog_bias %f,\n"
+               "    %s, ckpt(%s) abandoned,\n"
+               "    EPOCH DURATION: %.2fs\n") % (
+            epoch + 1,
+            tr_mag_loss, tr_logmag_loss, logbiasnet_lr, masknet_lr, val_logmag_loss, val_logmag_loss, np.mean(log_bias),
+            "NNET Rejected", ckpt_name, end_time - start_time)
+        tf.logging.info(msg)
+      with open(os.path.join(PARAM.SAVE_DIR, PARAM.CHECK_POINT+'_train.log'), 'a+') as f:
+        f.writelines(msg+'\n')
+
+      # Start halving when improvement is lower than start_halving_impr
+      if (rel_impr < PARAM.start_halving_impr) or (reject_num >= 2):
+        reject_num = 0
+        logbiasnet_lr *= PARAM.halving_factor
+        masknet_lr *= PARAM.halving_factor
+        tr_model.assign_lr(sess,logbiasnet_lr,masknet_lr)
+
+      # Stopping criterion
+      if rel_impr < PARAM.end_halving_impr:
+        if epoch < PARAM.min_epochs:
+          tf.logging.info(
+              "we were supposed to finish, but we continue as "
+              "min_epochs : %s" % PARAM.min_epochs)
+          continue
+        else:
+          tf.logging.info(
+              "finished, too small rel. improvement %g" % rel_impr)
+          break
+
+    sess.close()
+    tf.logging.info("Done training")
+
+
+def main(argv):
+  if not os.path.exists(PARAM.SAVE_DIR):
+    os.makedirs(PARAM.SAVE_DIR)
+  train()
+
+
+if __name__ == '__main__':
+  os.environ['CUDA_VISIBLE_DEVICES'] = sys.argv[1]
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run(main=main)
+  # python3 1_alter_train.py 1 2>&1 | tee exp/rnn_speech_enhancement/nnet_C00X_train_full.log
---
+++ models/training_in_turn_model.py
@@ -0,0 +1,433 @@
+# speech enhancement
+
+import tensorflow as tf
+import time
+import sys
+from tensorflow.contrib.rnn.python.ops import rnn
+from utils import tf_tool
+from losses import loss
+from utils.tf_tool import norm_mag_spec, norm_logmag_spec, rm_norm_mag_spec, rm_norm_logmag_spec
+from utils.tf_tool import normedLogmag2normedMag, normedMag2normedLogmag
+from utils.tf_tool import lstm_cell, GRU_cell
+import FLAGS
+import numpy as np
+
+
+def sum_attention(inputs, batch, input_finnal_dim):
+  '''
+  inputs: func inputs of calculate audio-suitable log_bias
+    dims" [batch,time,features]
+  '''
+  in_size = input_finnal_dim
+  with tf.variable_scope('sum_attention_fc'):
+    # attention layer
+    with tf.variable_scope('attention_scorer'):
+      weights_scorer = tf.get_variable('weights_scorer', [in_size, 1],
+                                       initializer=tf.random_normal_initializer(stddev=0.01))
+      biases_scorer = tf.get_variable('biases_scorer', [1],
+                                      initializer=tf.constant_initializer(0.0))
+      attention_alpha_vec = tf.matmul(tf.reshape(inputs, [-1, input_finnal_dim]),
+                                      weights_scorer) + biases_scorer  # [batch*time,1]
+      attention_alpha_vec = tf.reshape(attention_alpha_vec,[batch,1,-1]) # [batch,1,time]
+      attention_alpha_vec = tf.nn.softmax(attention_alpha_vec, axis=-1)
+      attened_vec = tf.reshape(tf.matmul(attention_alpha_vec, inputs), [batch,-1])# [batch,in_size]
+      return attened_vec
+
+
+class Training_Logbias_Model(object):
+  infer = 'infer'
+  train = 'train'
+  validation = 'validation'
+
+  def __init__(self,
+               x_mag_spec_batch,
+               lengths_batch,
+               y_mag_spec_batch=None,
+               theta_x_batch=None,
+               theta_y_batch=None,
+               behavior='train'):
+    '''
+    behavior = 'train/validation/infer'
+    '''
+    if behavior != self.infer:
+      assert(y_mag_spec_batch is not None)
+      assert(theta_x_batch is not None)
+      assert(theta_y_batch is not None)
+    self._x_mag_spec = x_mag_spec_batch
+    self._norm_x_mag_spec = norm_mag_spec(self._x_mag_spec, FLAGS.PARAM.MAG_NORM_MAX)
+
+    self._y_mag_spec = y_mag_spec_batch
+    self._norm_y_mag_spec = norm_mag_spec(self._y_mag_spec, FLAGS.PARAM.MAG_NORM_MAX)
+
+    self._lengths = lengths_batch
+    self._batch_size = tf.shape(self._lengths)[0]
+
+    self._x_theta = theta_x_batch
+    self._y_theta = theta_y_batch
+    self._model_type = FLAGS.PARAM.MODEL_TYPE
+
+    if FLAGS.PARAM.INPUT_TYPE == 'mag':
+      self.logbias_net_input = self._norm_x_mag_spec
+    elif FLAGS.PARAM.INPUT_TYPE == 'logmag':
+      tf.logging.error("Training_In_Turn_Model: NNET input must be magnitude spectrum.")
+      exit(-1)
+
+    # region training dropout
+    lstm_attn_cell = lstm_cell
+    if behavior != self.infer and FLAGS.PARAM.KEEP_PROB < 1.0:
+      def lstm_attn_cell(n_units, n_proj, act):
+        return tf.contrib.rnn.DropoutWrapper(lstm_cell(n_units, n_proj, act),
+                                             output_keep_prob=FLAGS.PARAM.KEEP_PROB)
+
+    GRU_attn_cell = GRU_cell
+    if behavior != self.infer and FLAGS.PARAM.KEEP_PROB < 1.0:
+      def GRU_attn_cell(n_units, act):
+        return tf.contrib.rnn.DropoutWrapper(GRU_cell(n_units, act),
+                                             output_keep_prob=FLAGS.PARAM.KEEP_PROB)
+    # endregion
+
+    # region logbias net
+    with tf.variable_scope('logbias_net'):
+      logbias_net_outputs = self.logbias_net_input
+      if FLAGS.PARAM.MODEL_TYPE.upper() == 'BLSTM':
+        with tf.variable_scope('BLSTM_logbias'):
+
+          lstm_fw_cell = tf.contrib.rnn.MultiRNNCell(
+              [lstm_attn_cell(FLAGS.PARAM.RNN_SIZE_LOGBIAS,
+                              FLAGS.PARAM.LSTM_num_proj_LOGBIAS,
+                              FLAGS.PARAM.LSTM_ACTIVATION_LOGBIAS)
+               for _ in range(FLAGS.PARAM.RNN_LAYER_LOGBIAS)], state_is_tuple=True)
+          lstm_bw_cell = tf.contrib.rnn.MultiRNNCell(
+              [lstm_attn_cell(FLAGS.PARAM.RNN_SIZE_LOGBIAS,
+                              FLAGS.PARAM.LSTM_num_proj_LOGBIAS,
+                              FLAGS.PARAM.LSTM_ACTIVATION_LOGBIAS)
+               for _ in range(FLAGS.PARAM.RNN_LAYER_LOGBIAS)], state_is_tuple=True)
+
+          fw_cell_logbiasnet = lstm_fw_cell._cells
+          bw_cell_logbiasnet = lstm_bw_cell._cells
+
+      if FLAGS.PARAM.MODEL_TYPE.upper() == 'BGRU':
+        with tf.variable_scope('BGRU_logbias'):
+
+          gru_fw_cell = tf.contrib.rnn.MultiRNNCell(
+              [GRU_attn_cell(FLAGS.PARAM.RNN_SIZE_LOGBIAS,
+                             FLAGS.PARAM.LSTM_ACTIVATION_LOGBIAS)
+               for _ in range(FLAGS.PARAM.RNN_LAYER_LOGBIAS)], state_is_tuple=True)
+          gru_bw_cell = tf.contrib.rnn.MultiRNNCell(
+              [GRU_attn_cell(FLAGS.PARAM.RNN_SIZE_LOGBIAS,
+                             FLAGS.PARAM.LSTM_ACTIVATION_LOGBIAS)
+               for _ in range(FLAGS.PARAM.RNN_LAYER_LOGBIAS)], state_is_tuple=True)
+
+          fw_cell_logbiasnet = gru_fw_cell._cells
+          bw_cell_logbiasnet = gru_bw_cell._cells
+
+      # dynamic rnn
+      result = rnn.stack_bidirectional_dynamic_rnn(
+          cells_fw=fw_cell_logbiasnet,
+          cells_bw=bw_cell_logbiasnet,
+          inputs=logbias_net_outputs,
+          dtype=tf.float32,
+          sequence_length=self._lengths)
+      logbias_net_outputs, fw_final_states, bw_final_states = result
+
+      logbias_biRnn_out_size = FLAGS.PARAM.RNN_SIZE_LOGBIAS*2
+      attend_fea = sum_attention(logbias_net_outputs,self._batch_size,logbias_biRnn_out_size)
+
+      with tf.variable_scope('fullconnectSuitableLogbias'):
+        weights_logbias_fc = tf.get_variable('weights_logbias_fc', [logbias_biRnn_out_size, 1],
+                                             initializer=tf.random_normal_initializer(stddev=0.01))
+        biases_logbias_fc = tf.get_variable('biases_logbias_fc', [1],
+                                            initializer=tf.constant_initializer(0.0))
+        logbias_net_out = tf.expand_dims(
+          tf.matmul(attend_fea, weights_logbias_fc) + biases_logbias_fc, axis=-1)  # [batch,1,1]
+        self._log_bias = tf.nn.relu(logbias_net_out+FLAGS.PARAM.INIT_LOG_BIAS)
+
+      self._real_logbias = tf.add(self._log_bias, FLAGS.PARAM.MIN_LOG_BIAS)
+    # endregion
+
+    self._norm_x_logmag_spec = norm_logmag_spec(self._x_mag_spec, FLAGS.PARAM.MAG_NORM_MAX, self._log_bias, FLAGS.PARAM.MIN_LOG_BIAS)
+    self._norm_y_logmag_spec = norm_logmag_spec(self._y_mag_spec, FLAGS.PARAM.MAG_NORM_MAX, self._log_bias, FLAGS.PARAM.MIN_LOG_BIAS)
+
+    # region mask net
+    with tf.variable_scope('mask_net'):
+      mask_net_outputs = self._norm_x_logmag_spec
+      if FLAGS.PARAM.MODEL_TYPE.upper() == 'BLSTM':
+        with tf.variable_scope('BLSTM_mask'):
+
+          lstm_fw_cell = tf.contrib.rnn.MultiRNNCell(
+              [lstm_attn_cell(FLAGS.PARAM.RNN_SIZE_MASK,
+                              FLAGS.PARAM.LSTM_num_proj_MASK,
+                              FLAGS.PARAM.LSTM_ACTIVATION_MASK)
+               for _ in range(FLAGS.PARAM.RNN_LAYER_MASK)], state_is_tuple=True)
+          lstm_bw_cell = tf.contrib.rnn.MultiRNNCell(
+              [lstm_attn_cell(FLAGS.PARAM.RNN_SIZE_MASK,
+                              FLAGS.PARAM.LSTM_num_proj_MASK,
+                              FLAGS.PARAM.LSTM_ACTIVATION_MASK)
+               for _ in range(FLAGS.PARAM.RNN_LAYER_MASK)], state_is_tuple=True)
+
+          fw_cell_masknet = lstm_fw_cell._cells
+          bw_cell_masknet = lstm_bw_cell._cells
+
+      if FLAGS.PARAM.MODEL_TYPE.upper() == 'BGRU':
+        with tf.variable_scope('BGRU_mask'):
+
+          gru_fw_cell = tf.contrib.rnn.MultiRNNCell(
+              [GRU_attn_cell(FLAGS.PARAM.RNN_SIZE,
+                             FLAGS.PARAM.LSTM_ACTIVATION) for _ in range(FLAGS.PARAM.RNN_LAYER)], state_is_tuple=True)
+          gru_bw_cell = tf.contrib.rnn.MultiRNNCell(
+              [GRU_attn_cell(FLAGS.PARAM.RNN_SIZE,
+                             FLAGS.PARAM.LSTM_ACTIVATION) for _ in range(FLAGS.PARAM.RNN_LAYER)], state_is_tuple=True)
+
+          fw_cell_masknet = gru_fw_cell._cells
+          bw_cell_masknet = gru_bw_cell._cells
+
+      # dynamic rnn
+      result = rnn.stack_bidirectional_dynamic_rnn(
+          cells_fw=fw_cell_masknet,
+          cells_bw=bw_cell_masknet,
+          inputs=mask_net_outputs,
+          dtype=tf.float32,
+          sequence_length=self._lengths)
+
+      mask_net_outputs, fw_final_states, bw_final_states = result
+      mask_biRnn_output_size = FLAGS.PARAM.RNN_SIZE_MASK*2
+      flatten_outputs = tf.reshape(mask_net_outputs, [-1, mask_biRnn_output_size])
+      out_size = FLAGS.PARAM.OUTPUT_SIZE
+      with tf.variable_scope('fullconnectMask'):
+        weights = tf.get_variable('weights1', [mask_biRnn_output_size, out_size],
+                                  initializer=tf.random_normal_initializer(stddev=0.01))
+        biases = tf.get_variable('biases1', [out_size],
+                                 initializer=tf.constant_initializer(0.0))
+      mask = tf.nn.relu(tf.matmul(flatten_outputs, weights) + biases)
+      self._mask = tf.reshape(
+          mask, [self._batch_size, -1, FLAGS.PARAM.OUTPUT_SIZE])
+    # endregion
+
+    # region prepare y_estimation and y_labels
+    self._y_mag_labels = self._norm_y_mag_spec
+    self._y_logmag_labels = self._norm_y_logmag_spec
+    if FLAGS.PARAM.TRAINING_MASK_POSITION == 'mag':
+      self._y_normed_mag_estimation = self._mask*self._norm_x_mag_spec
+      self._y_normed_logmag_estimation = normedMag2normedLogmag(self._y_normed_mag_estimation, FLAGS.PARAM.MAG_NORM_MAX,
+                                                                self._log_bias, FLAGS.PARAM.MIN_LOG_BIAS)
+    elif FLAGS.PARAM.TRAINING_MASK_POSITION == 'logmag':
+      self._y_normed_logmag_estimation = self._mask*self._norm_x_logmag_spec
+      self._y_normed_mag_estimation = normedLogmag2normedMag(self._y_normed_logmag_estimation, FLAGS.PARAM.MAG_NORM_MAX,
+                                                             self._log_bias, FLAGS.PARAM.MIN_LOG_BIAS)
+    if FLAGS.PARAM.MASK_TYPE == 'PSM':
+      self._y_mag_labels *= tf.cos(self._x_theta-self._y_theta)
+      self._y_logmag_labels *= tf.cos(self._x_theta-self._y_theta)
+    elif FLAGS.PARAM.MASK_TYPE == 'IRM':
+      pass
+    else:
+      tf.logging.error('Mask type error.')
+      exit(-1)
+
+    # region get infer spec
+    if FLAGS.PARAM.DECODING_MASK_POSITION != FLAGS.PARAM.TRAINING_MASK_POSITION:
+      print('Error, DECODING_MASK_POSITION should be equal to TRAINING_MASK_POSITION when use training_in_turn_model.')
+    if FLAGS.PARAM.DECODING_MASK_POSITION == 'mag':
+      self._y_mag_estimation = rm_norm_mag_spec(self._y_normed_mag_estimation, FLAGS.PARAM.MAG_NORM_MAX)
+    elif FLAGS.PARAM.DECODING_MASK_POSITION == 'logmag':
+      self._y_mag_estimation = rm_norm_logmag_spec(self._y_normed_logmag_estimation,
+                                                   FLAGS.PARAM.MAG_NORM_MAX,
+                                                   self._log_bias, FLAGS.PARAM.MIN_LOG_BIAS)
+    '''
+    _y_mag_estimation is estimated mag_spec
+    '''
+    # endregion
+
+    # endregion
+
+    self.saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=30)
+
+    if behavior == self.infer:
+      return
+
+    # region get LOSS
+    if FLAGS.PARAM.LOSS_FUNC == 'SPEC_MSE':  # log_mag and mag MSE
+      self._logbiasnet_loss = loss.reduce_sum_frame_batchsize_MSE(
+          self._y_normed_mag_estimation, self._y_mag_labels)
+      self._masknet_loss = loss.reduce_sum_frame_batchsize_MSE(
+        self._y_normed_logmag_estimation,self._y_logmag_labels)
+    elif FLAGS.PARAM.LOSS_FUNC == "SPEC_MSE_FLEXIBLE_POW_C":
+      self._logbiasnet_loss = loss.reduce_sum_frame_batchsize_MSE_EmphasizeLowerValue(self._y_normed_mag_estimation,
+                                                                                      self._y_mag_labels,
+                                                                                      FLAGS.PARAM.POW_COEF)
+      self._masknet_loss = loss.reduce_sum_frame_batchsize_MSE_EmphasizeLowerValue(self._y_normed_logmag_estimation,
+                                                                                   self._y_logmag_labels,
+                                                                                   FLAGS.PARAM.POW_COEF)
+    else:
+      print('Loss type error.')
+      exit(-1)
+    # endregion
+
+    if behavior == self.validation:
+      '''
+      val model cannot train.
+      '''
+      return
+    self._lr_logbiasnet = tf.Variable(0.0, trainable=False)
+    self._lr_masknet = tf.Variable(0.0, trainable=False)
+    tvars = tf.trainable_variables()
+    logbias_vars = [var for var in tvars if 'logbias_net' in var.name]
+    mask_vars = [var for var in tvars if 'mask_net' in var.name]
+    logbiasnet_grads, _ = tf.clip_by_global_norm(tf.gradients(self._logbiasnet_loss, logbias_vars),
+                                                 FLAGS.PARAM.CLIP_NORM)
+    masknet_grads, _ = tf.clip_by_global_norm(tf.gradients(self._masknet_loss, mask_vars),
+                                              FLAGS.PARAM.CLIP_NORM)
+    optimizer_logbiasnet = tf.train.AdamOptimizer(self.lr_logbiasnet)
+    optimizer_masknet = tf.train.AdamOptimizer(self.lr_masknet)
+    #optimizer = tf.train.GradientDescentOptimizer(self.lr)
+    # all_grads = [grad for grad in logbiasnet_grads]
+    # for grad in masknet_grads:
+    #   all_grads.append(grad)
+    # all_vars = [var for var in logbias_vars]
+    # for var in mask_vars:
+    #   all_vars.append(var)
+    train_logbiasnet = optimizer_logbiasnet.apply_gradients(zip(logbiasnet_grads, logbias_vars))
+    train_masknet = optimizer_masknet.apply_gradients(zip(masknet_grads, mask_vars))
+    self._train_op = [train_logbiasnet, train_masknet]
+
+    self._new_lr_logbiasnet = tf.placeholder(
+        tf.float32, shape=[], name='new_learning_rate1')
+    self._new_lr_masknet = tf.placeholder(
+        tf.float32, shape=[], name='new_learning_rate2')
+    self._lr_update = [tf.assign(self._lr_logbiasnet, self._new_lr_logbiasnet),
+                       tf.assign(self._lr_masknet, self._new_lr_masknet)]
+
+  def assign_lr(self, session, lr_logbiasnet, lr_masknet):
+    session.run(self._lr_update, feed_dict={self._new_lr_logbiasnet: lr_logbiasnet,
+                                            self._new_lr_masknet: lr_masknet})
+
+  @property
+  def log_bias(self):
+    '''
+    description: logbias of log(x+logbias)
+    type: >0
+    dims: [batch,1,1]
+    '''
+    return self._real_logbias
+
+
+  @property
+  def y_mag_estimation(self):
+    '''
+    description: model outputs
+    type: enhanced spectrum
+    dims: [batch,time,frequence]
+    '''
+    return self._y_mag_estimation
+
+  @property
+  def x_mag(self):
+    '''
+    description: model inputs
+    type: mixture spectrum
+    dims: [batch,time,frequence]
+    '''
+    return self._x_mag_spec
+
+  @property
+  def y_mag(self):
+    '''
+    description: trainning reference
+    type: clean spectrum
+    dims: [batch,time,frequence]
+    '''
+    return self._y_mag_spec
+
+  @property
+  def mask(self):
+    '''
+    description: wiener filtering mat
+    type:
+    dims: same to spectrum
+    '''
+    return self._mask
+
+  @property
+  def x_theta(self):
+    '''
+    description: angle of inputs
+    type:
+    dims: [batch, time, frequence]
+    '''
+    return self._x_theta
+
+  @property
+  def y_theta(self):
+    '''
+    description: angle of labels
+    type:
+    dims: [batch, time, frequence]
+    '''
+    return self._y_theta
+
+  @property
+  def lengths(self):
+    '''
+    description: dynamic time length
+    type: an int list
+    dims: [batch]
+    '''
+    return self._lengths
+
+  @property
+  def batch_size(self):
+    '''
+    description: dynamic batch_size
+    type: an int number
+    dims: [0]
+    '''
+    return self._batch_size
+
+
+  @property
+  def lr_logbiasnet(self):
+    '''
+    description: learning rate
+    type:
+    dims: [0]
+    '''
+    return self._lr_logbiasnet
+
+
+  @property
+  def lr_masknet(self):
+    '''
+    description: learning rate
+    type:
+    dims: [0]
+    '''
+    return self._lr_masknet
+
+
+  @property
+  def logbiasnet_loss(self):
+    '''
+    description: model loss1
+    type:
+    dims: [0]
+    '''
+    return self._logbiasnet_loss
+
+
+  @property
+  def masknet_loss(self):
+    '''
+    description: model loss2
+    type:
+    dims: [0]
+    '''
+    return self._masknet_loss
+
+
+  @property
+  def train_op(self):
+    '''
+    description: training operation node
+    type: tensorflow computation node
+    dims:
+    '''
+    return self._train_op
