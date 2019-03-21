08c5367b1d16f29ce97b49d45acc28ed198aaaf5
IMLHF <red_wind@foxmail.com>
2019-03-21 12:57:59 +0800

    # update alter train script

--- 1_alter_train.py
+++ 1_alter_train.py
@@ -188,14 +188,18 @@ def train():
       ckpt_path = os.path.join(ckpt_dir, ckpt_name)
 
       # Relative loss between previous and current val_loss
-      rel_impr = np.abs(mag_loss_prev - val_mag_loss) / mag_loss_prev
+      rel_impr_magloss = np.abs(mag_loss_prev - val_mag_loss) / mag_loss_prev
+      rel_impr_logmagloss = np.abs(logmag_loss_prev - val_logmag_loss) / logmag_loss_prev
       # Accept or reject new parameters
       msg = ""
-      if val_mag_loss < mag_loss_prev:
+      if (PARAM.TRAIN_TYPE == 'BOTH' and (val_mag_loss < mag_loss_prev) and (val_logmag_loss < logmag_loss_prev)) or (
+              PARAM.TRAIN_TYPE == 'LOGBIASNET' and (val_mag_loss < mag_loss_prev)) or (
+              PARAM.TRAIN_TYPE == 'MASKNET' and (val_logmag_loss < logmag_loss_prev)):
         reject_num = 0
         tr_model.saver.save(sess, ckpt_path)
         # Logging train loss along with validation loss
         mag_loss_prev = val_mag_loss
+        logmag_loss_prev = val_logmag_loss
         best_path = ckpt_path
         msg = ("Train Iteration %03d: \n"
                "    Train.LOSS (%.4f,%.4f), lrate(%e,%e), Val.LOSS (%.4f,%.4f), avglog_bias %f,\n"
@@ -220,14 +224,28 @@ def train():
         f.writelines(msg+'\n')
 
       # Start halving when improvement is lower than start_halving_impr
-      if (rel_impr < PARAM.start_halving_impr) or (reject_num >= 2):
-        reject_num = 0
-        logbiasnet_lr *= PARAM.halving_factor
-        masknet_lr *= PARAM.halving_factor
-        tr_model.assign_lr(sess,logbiasnet_lr,masknet_lr)
+      if PARAM.TRAIN_TYPE == 'BOTH':
+        if (rel_impr_magloss < PARAM.start_halving_impr) or (rel_impr_logmagloss < PARAM.start_halving_impr) or (reject_num >= 2):
+          reject_num = 0
+          logbiasnet_lr *= PARAM.halving_factor
+          masknet_lr *= PARAM.halving_factor
+          tr_model.assign_lr(sess,logbiasnet_lr,masknet_lr)
+      elif PARAM.TRAIN_TYPE == 'LOGBIASNET':
+        if (rel_impr_magloss < PARAM.start_halving_impr) or (reject_num >= 2):
+          reject_num = 0
+          logbiasnet_lr *= PARAM.halving_factor
+          tr_model.assign_lr(sess,logbiasnet_lr,masknet_lr)
+      elif PARAM.TRAIN_TYPE == 'MASKNET':
+        if (rel_impr_logmagloss < PARAM.start_halving_impr) or (reject_num >= 2):
+          reject_num = 0
+          masknet_lr *= PARAM.halving_factor
+          tr_model.assign_lr(sess,logbiasnet_lr,masknet_lr)
+      else:
+        print('Train type error.')
+        exit(-1)
 
       # Stopping criterion
-      if rel_impr < PARAM.end_halving_impr:
+      if rel_impr_magloss < PARAM.end_halving_impr and rel_impr_logmagloss < PARAM.end_halving_impr:
         if epoch < PARAM.min_epochs:
           tf.logging.info(
               "we were supposed to finish, but we continue as "
@@ -235,7 +253,7 @@ def train():
           continue
         else:
           tf.logging.info(
-              "finished, too small rel. improvement %g" % rel_impr)
+              "finished, too small rel. improvement (%g,%g)" % (rel_impr_magloss, rel_impr_logmagloss))
           break
 
     sess.close()
