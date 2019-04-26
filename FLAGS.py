413a60fa3104c8fc705c7aa5dad220d0226a9341
IMLHF <red_wind@foxmail.com>
2019-04-27 01:25:34 +0800

    # add

--- FLAGS.py
+++ FLAGS.py
@@ -464,7 +464,7 @@ class C_RealPSM_RelativeLoss2AFD100_LB1_2(base_config): # DONE 15123
   # MASK_TYPE = "PSM" # default
 
 
-class C_RealPSM_RelativeLoss2AFD1000_LB1_2(base_config): # RUNNING 15123
+class C_RealPSM_RelativeLoss2AFD1000_LB1_2(base_config): # DONE 15123
   '''
   relative spectrum(mag) MSE v2
   [(y-y_)^1.2/(|y|+|y_|)/ignore_coef(|y|+|y_|)]^2
@@ -476,6 +476,23 @@ class C_RealPSM_RelativeLoss2AFD1000_LB1_2(base_config): # RUNNING 15123
   DECODING_MASK_POSITION = TRAINING_MASK_POSITION
   LOSS_FUNC_FOR_MAG_SPEC = "AUTO_RELATED_MSE2"
   AUTO_RELATED_MSE_AXIS_FIT_DEG = 1000
+  LINEAR_BROKER = 1.2
+  ReLU_MASK = False
+  # MASK_TYPE = "PSM" # default
+
+
+class C_RealPSM_RelativeLoss2AFD2000_LB1_2(base_config): # RUNNING 15123
+  '''
+  relative spectrum(mag) MSE v2
+  [(y-y_)^1.2/(|y|+|y_|)/ignore_coef(|y|+|y_|)]^2
+  '''
+  CHECK_POINT = 'nnet_C_RealPSM_RelativeLoss2AFD2000_LB1_2'
+  INPUT_TYPE = 'mag'  # 'mag' or 'logmag'
+  LABEL_TYPE = 'mag'  # 'mag' or 'logmag'
+  TRAINING_MASK_POSITION = 'mag'  # 'mag' or 'logmag'
+  DECODING_MASK_POSITION = TRAINING_MASK_POSITION
+  LOSS_FUNC_FOR_MAG_SPEC = "AUTO_RELATED_MSE2"
+  AUTO_RELATED_MSE_AXIS_FIT_DEG = 2000
   LINEAR_BROKER = 1.2
   ReLU_MASK = False
   # MASK_TYPE = "PSM" # default
@@ -632,6 +649,44 @@ class C_RealPSM_RelativeLoss3_008(base_config): # RUNNING 15123
   ReLU_MASK = False
   # MASK_TYPE = "PSM" # default
 
+
+class C_RealPSM_RelativeLoss3_009(base_config): # RUNNING 15123
+  '''
+  relative spectrum(mag) MSE v3
+  [|y-y_|/((A*|y|)^C1+B)]^C2
+  '''
+  CHECK_POINT = 'nnet_C_RealPSM_RelativeLoss3_009'
+  INPUT_TYPE = 'mag'  # 'mag' or 'logmag'
+  LABEL_TYPE = 'mag'  # 'mag' or 'logmag'
+  TRAINING_MASK_POSITION = 'mag'  # 'mag' or 'logmag'
+  DECODING_MASK_POSITION = TRAINING_MASK_POSITION
+  LOSS_FUNC_FOR_MAG_SPEC = "AUTO_RELATED_MSE3"
+  AUTO_RELATIVE_LOSS3_A = 1.5 # A: [1左右] 控制关注程度的变化速度，越大损失函数的开口变化速度越快
+  AUTO_RELATIVE_LOSS3_B = 0.05 # B: [0.001-0.2] 控制|y|趋向于无穷小时的开口大小,B越小开口越小;开口太小不收敛,开口太大效果差(小值的关注度不够)
+  AUTO_RELATIVE_LOSS3_C1 = 1.1 # C1: 为1时, 底部等高线为直线; 小于1时向外弯曲, 类似x^2; 大于1时向内弯曲.
+  AUTO_RELATIVE_LOSS3_C2 = 2.0 # C2: 次幂,越大容错率越大,容易收敛,收敛后效果较差;越小容错率越小,不以收敛,收敛后效果较好.
+  ReLU_MASK = False
+  # MASK_TYPE = "PSM" # default
+
+
+class C_RealPSM_RelativeLoss3_010(base_config): # RUNNING 15123
+  '''
+  relative spectrum(mag) MSE v3
+  [|y-y_|/((A*|y|)^C1+B)]^C2
+  '''
+  CHECK_POINT = 'nnet_C_RealPSM_RelativeLoss3_010'
+  INPUT_TYPE = 'mag'  # 'mag' or 'logmag'
+  LABEL_TYPE = 'mag'  # 'mag' or 'logmag'
+  TRAINING_MASK_POSITION = 'mag'  # 'mag' or 'logmag'
+  DECODING_MASK_POSITION = TRAINING_MASK_POSITION
+  LOSS_FUNC_FOR_MAG_SPEC = "AUTO_RELATED_MSE3"
+  AUTO_RELATIVE_LOSS3_A = 1.5 # A: [1左右] 控制关注程度的变化速度，越大损失函数的开口变化速度越快
+  AUTO_RELATIVE_LOSS3_B = 0.05 # B: [0.001-0.2] 控制|y|趋向于无穷小时的开口大小,B越小开口越小;开口太小不收敛,开口太大效果差(小值的关注度不够)
+  AUTO_RELATIVE_LOSS3_C1 = 0.9 # C1: 为1时, 底部等高线为直线; 小于1时向外弯曲, 类似x^2; 大于1时向内弯曲.
+  AUTO_RELATIVE_LOSS3_C2 = 2.0 # C2: 次幂,越大容错率越大,容易收敛,收敛后效果较差;越小容错率越小,不以收敛,收敛后效果较好.
+  ReLU_MASK = False
+  # MASK_TYPE = "PSM" # default
+
 # 初始的ReLoss_MSE，对应的A大一些，B也大一些，C1=1.0
 
 class C_RealPSM_RelativeLoss4AFD100_001(base_config): # DONE 15123
@@ -668,7 +723,7 @@ class C_RealPSM_RelativeLoss4AFD100_002(base_config): # DONE 15123
   # MASK_TYPE = "PSM" # default
 
 
-class C_RealPSM_RelativeLoss4AFD100_003(base_config): # RUNNING 15123
+class C_RealPSM_RelativeLoss4AFD100_003(base_config): # DONE 15123
   '''
   relative spectrum(mag) MSE v4
   [(y-y_)/(|y|+relu(sign(y)*y_))/ignore_coef(|y|+|y_|)]^2
@@ -700,5 +755,5 @@ class C_RealPSM_RelativeLoss5(base_config): # DONE 15123
   # MASK_TYPE = "PSM" # default
 
 
-PARAM = C_RealPSM_RelativeLoss3_008
+PARAM = C_RealPSM_RelativeLoss3_010
 # print(PARAM.TRAINING_MASK_POSITION != PARAM.LABEL_TYPE)
